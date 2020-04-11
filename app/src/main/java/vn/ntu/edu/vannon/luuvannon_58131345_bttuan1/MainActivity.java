ae621ea85997cea790de9ee8c58097a720e1d33d
LuuVanNon <votong926v@gmail.com>
2020-04-11 17:24:48 +0700

    Initial commit

---
+++ app/src/main/java/vn/ntu/edu/vannon/luuvannon_58131345_bttuan1/MainActivity.java
@@ -0,0 +1,14 @@
+package vn.ntu.edu.vannon.luuvannon_58131345_bttuan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
